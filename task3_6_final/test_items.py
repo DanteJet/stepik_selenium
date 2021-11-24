b081b093b9944b6f270b478335b01c7864b0c264
Yakov Bubnov <y.bubnov@ispsystem.com>
2021-11-24 12:19:20 +0800

    add new task from 3_6 module

--- task3_6_final/test_items.py
+++ task3_6_final/test_items.py
@@ -4,7 +4,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 class TestMultilanguage:
-    def test_items(self, browser):
+    def test_items_add_to_cart_button(self, browser):
         link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
         browser.get(link)
         button = WebDriverWait(browser, 8).until(
